ed2e8f7de6b1bb3f7a22b692d7ff0fd46e0d5286
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-05-16 21:26:22 +0300

    Added Kendo.Mvc

--- Movies/Movies/App_Start/BundleConfig.cs
+++ Movies/Movies/App_Start/BundleConfig.cs
@@ -10,6 +10,10 @@ namespace Movies.Web.App_Start
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/Kendo/kendo.web.min.js",
+                "~/Scripts/Kendo/kendo.aspnetmvc.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -25,6 +29,10 @@ namespace Movies.Web.App_Start
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo-css").Include(
+                "~/Content/Kendo/kendo.black.min.css",
+                "~/Content/Kendo/kendo.common.min.css"));
         }
     }
 }
