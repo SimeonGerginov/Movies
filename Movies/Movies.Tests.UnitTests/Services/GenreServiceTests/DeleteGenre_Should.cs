a447776f6cbaa774180376e045056b7da6d91895
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-05-28 22:41:33 +0300

    Added GenreService tests

---
+++ Movies/Movies.Tests.UnitTests/Services/GenreServiceTests/DeleteGenre_Should.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using Moq;
+
+using Movies.Core.Contracts;
+using Movies.Core.Models;
+using Movies.Services;
+
+using NUnit.Framework;
+
+namespace Movies.Tests.UnitTests.Services.GenreServiceTests
+{
+    [TestFixture]
+    public class DeleteGenre_Should
+    {
+        [Test]
+        public void ReturnFalse_WhenPassedGenreDoesNotExist()
+        {
+            // Arrange
+            var genreMock = new Mock<Genre>();
+            var genreRepositoryMock = new Mock<IRepository<Genre>>();
+            var genreService = new GenreService(genreRepositoryMock.Object);
+
+            IEnumerable<Genre> filteredGenres = new List<Genre>();
+            
+            genreRepositoryMock.Setup(gr => gr.GetAllFiltered(It.IsAny<Expression<Func<Genre, bool>>>()))
+                .Returns(filteredGenres);
+
+            // Act
+            var result = genreService.DeleteGenre(genreMock.Object.Name);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void CallDeleteMethodOfRepositoryOnce_WhenPassedGenreExists()
+        {
+            // Arrange
+            var genreMock = new Mock<Genre>();
+            var genreRepositoryMock = new Mock<IRepository<Genre>>();
+            var genreService = new GenreService(genreRepositoryMock.Object);
+
+            ICollection<Genre> filteredGenres = new List<Genre>();
+            filteredGenres.Add(genreMock.Object);
+
+            genreRepositoryMock.Setup(gr => gr.GetAllFiltered(It.IsAny<Expression<Func<Genre, bool>>>()))
+                .Returns(filteredGenres);
+
+            // Act
+            genreService.DeleteGenre(genreMock.Object.Name);
+
+            // Assert
+            genreRepositoryMock.Verify(gr => gr.Delete(genreMock.Object), Times.Once);
+        }
+
+        [Test]
+        public void ReturnTrue_WhenPassedGenreExists()
+        {
+            // Arrange
+            var genreMock = new Mock<Genre>();
+            var genreRepositoryMock = new Mock<IRepository<Genre>>();
+            var genreService = new GenreService(genreRepositoryMock.Object);
+
+            ICollection<Genre> filteredGenres = new List<Genre>();
+            filteredGenres.Add(genreMock.Object);
+
+            genreRepositoryMock.Setup(gr => gr.GetAllFiltered(It.IsAny<Expression<Func<Genre, bool>>>()))
+                .Returns(filteredGenres);
+
+            // Act
+            var result = genreService.DeleteGenre(genreMock.Object.Name);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Movies/Movies.Tests.UnitTests/Services/GenreServiceTests/GetAllGenres_Should.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Moq;
+
+using Movies.Core.Contracts;
+using Movies.Core.Models;
+using Movies.Services;
+
+using NUnit.Framework;
+
+namespace Movies.Tests.UnitTests.Services.GenreServiceTests
+{
+    [TestFixture]
+    public class GetAllGenres_Should
+    {
+        [Test]
+        public void ReturnAllGenresFromRepository()
+        {
+            // Arrange
+            var genreMock = new Mock<Genre>();
+            var genreRepositoryMock = new Mock<IRepository<Genre>>();
+            var genreService = new GenreService(genreRepositoryMock.Object);
+
+            ICollection<Genre> genresFromRepository = new List<Genre>();
+            genresFromRepository.Add(genreMock.Object);
+
+            genreRepositoryMock.Setup(gr => gr.GetAll())
+                .Returns(genresFromRepository);
+
+            // Act
+            var genres = genreService.GetAllGenres();
+
+            // Assert
+            Assert.AreEqual(genres.Count(), genresFromRepository.Count);
+            CollectionAssert.Contains(genres, genreMock.Object);
+        }
+    }
+}
