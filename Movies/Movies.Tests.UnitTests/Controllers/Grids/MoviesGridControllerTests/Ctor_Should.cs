5611ad59164d3798fad27b045a0e2bbc28a9c8b9
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-06-29 12:56:50 +0300

    Added MoviesGridController tests

---
+++ Movies/Movies.Tests.UnitTests/Controllers/Grids/MoviesGridControllerTests/Ctor_Should.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using AutoMapper;
+using Moq;
+
+using Movies.Services.Contracts;
+using Movies.Web.Areas.Admin.Controllers.Grids;
+
+using NUnit.Framework;
+
+namespace Movies.Tests.UnitTests.Controllers.Grids.MoviesGridControllerTests
+{
+    [TestFixture]
+    public class Ctor_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WhenPassedMovieServiceIsNull()
+        {
+            // Arrange
+            var fileConverterMock = new Mock<IFileConverter>();
+            var mapperMock = new Mock<IMapper>();
+
+            // Act && Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new MoviesGridController(null, fileConverterMock.Object, mapperMock.Object));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenPassedFileConverterIsNull()
+        {
+            // Arrange
+            var movieServiceMock = new Mock<IMovieService>();
+            var mapperMock = new Mock<IMapper>();
+
+            // Act && Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new MoviesGridController(movieServiceMock.Object, null, mapperMock.Object));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenPassedMapperIsNull()
+        {
+            // Arrange
+            var movieServiceMock = new Mock<IMovieService>();
+            var fileConverterMock = new Mock<IFileConverter>();
+
+            // Act && Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new MoviesGridController(movieServiceMock.Object, fileConverterMock.Object, null));
+        }
+
+        [Test]
+        public void CreateAnInstanceOfMoviesGridController_WhenPassedArgumentsAreNotNull()
+        {
+            // Arrange
+            var movieServiceMock = new Mock<IMovieService>();
+            var fileConverterMock = new Mock<IFileConverter>();
+            var mapperMock = new Mock<IMapper>();
+
+            // Act
+            var moviesGridController = 
+                new MoviesGridController(movieServiceMock.Object, fileConverterMock.Object, mapperMock.Object);
+
+            // Assert
+            Assert.IsInstanceOf<MoviesGridController>(moviesGridController);
+        }
+    }
+}
---
+++ Movies/Movies.Tests.UnitTests/Controllers/Grids/MoviesGridControllerTests/GetImage_Should.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using Moq;
+
+using Movies.Core.Models;
+using Movies.Services.Contracts;
+using Movies.Web.Areas.Admin.Controllers.Grids;
+using Movies.Web.ViewModels.Grid;
+
+using NUnit.Framework;
+
+namespace Movies.Tests.UnitTests.Controllers.Grids.MoviesGridControllerTests
+{
+    [TestFixture]
+    public class GetImage_Should
+    {
+        [Test]
+        public void ReturnFileContentResult_WhenPictureWithThePassedIdExists()
+        {
+            // Arrange 
+            var movieServiceMock = new Mock<IMovieService>();
+            var fileConverterMock = new Mock<IFileConverter>();
+            var mapperMock = new Mock<IMapper>();
+            var pictureId = "1";
+
+            var gridMovieViewModel = new GridMovieViewModel()
+            {
+                Id = 1,
+                Name = "Movie name",
+                Image = null,
+                Year = "1980",
+                RunningTime = 120,
+                GenreName = "Genre name"
+            };
+
+            var movieDbModel = new Movie()
+            {
+                Name = gridMovieViewModel.Name,
+                Image = gridMovieViewModel.Image,
+                Year = gridMovieViewModel.Year,
+                RunningTime = gridMovieViewModel.RunningTime
+            };
+
+            var moviesList = new List<Movie>() { movieDbModel };
+            movieServiceMock.Setup(ms => ms.GetAllMovies()).Returns(moviesList);
+
+            mapperMock.Setup(x => x.Map<Movie>(gridMovieViewModel)).Returns(movieDbModel);
+            mapperMock.Setup(x => x.Map<GridMovieViewModel>(movieDbModel)).Returns(gridMovieViewModel);
+
+            // Act
+            var moviesGridController =
+                new MoviesGridController(movieServiceMock.Object, fileConverterMock.Object, mapperMock.Object);
+            var picture = moviesGridController.GetImage(pictureId);
+
+            // Assert
+            Assert.IsNotNull(picture);
+        }
+    }
+}
